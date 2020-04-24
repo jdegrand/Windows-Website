5713f86122a995061eda881fc41a2a62c0424a8a
Joe DeGrand <degrandjoe@gmail.com>
2020-04-24 16:48:53 -0400

    Updated boundries

--- dragging.js
+++ dragging.js
@@ -1,59 +1,77 @@
-/* The following code is from W3 Schools
+/* The following code is from W3 Schools and Stack Overflow, with slight modifications by me
    https://www.w3schools.com/howto/howto_js_draggable.asp */
 
-//Make the DIV element draggagle:
-dragElement(document.getElementById("draggable_window"));
+   var PADDING = 0;
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    /* if present, the header is where you move the DIV from:*/
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    /* otherwise, move the DIV from anywhere inside the DIV:*/
-    elmnt.onmousedown = dragMouseDown;
-  }
-
-  function dragMouseDown(e) {
-    bringElementToFront(elmnt);
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    var boundRect = document.getElementById("screen").getBoundingClientRect();
-    console.log("Left", boundRect.left, "Right", boundRect.right, "Top", boundRect.top, "Bottom", boundRect.bottom)
-    console.log("Width", elmnt.offsetWidth);
-    console.log("Right", elmnt.style.right)
-    if ((elmnt.offsetTop - pos2) >= boundRect.top) {
-      if (elmnt.offsetTop - pos2 + elmnt.offsetHeight <= boundRect.bottom) {
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-      }
-    }
-    console.log(elmnt.offsetLeft - pos1 + elmnt.offsetWidth, boundRect.right, elmnt.offsetWidth);
-    if ((elmnt.offsetLeft - pos1) >= boundRect.top) {
-      if (elmnt.offsetLeft - pos1 + elmnt.offsetWidth <= boundRect.right) {
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-      }
-    }
-  }
-  function closeDragElement() {
-    /* stop moving when mouse button is released:*/
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+   var rect;
+   var viewport = {
+     bottom: 0,
+     left: 0,
+     right: 0,
+     top: 0
+   }
+   
+   //Make the DIV element draggagle:
+   dragElement(document.getElementById("draggable_window"));
+   
+   function dragElement(elmnt) {
+     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+     if (document.getElementById(elmnt.id + "header")) {
+       /* if present, the header is where you move the DIV from:*/
+       document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+     } else {
+       /* otherwise, move the DIV from anywhere inside the DIV:*/
+       elmnt.onmousedown = dragMouseDown;
+     }
+   
+     function dragMouseDown(e) {
+      if(e.preventDefault) e.preventDefault();
+       bringElementToFront(elmnt);
+       e = e || window.event;
+       // get the mouse cursor position at startup:
+       pos3 = e.clientX;
+       pos4 = e.clientY;
+       
+       // store the current viewport and element dimensions when a drag starts
+       rect = elmnt.getBoundingClientRect();
+       viewport.bottom = window.innerHeight - PADDING;
+       viewport.left = PADDING;
+       viewport.right = window.innerWidth - PADDING;
+       viewport.top = PADDING;
+       
+       document.onmouseup = closeDragElement;
+       // call a function whenever the cursor moves:
+       document.onmousemove = elementDrag;
+     }
+   
+     function elementDrag(e) {
+       e = e || window.event;
+       // calculate the new cursor position:
+       pos1 = pos3 - e.clientX;
+       pos2 = pos4 - e.clientY;
+       pos3 = e.clientX;
+       pos4 = e.clientY;
+       
+       // check to make sure the element will be within our viewport boundary
+       var newLeft = elmnt.offsetLeft - pos1;
+       var newTop = elmnt.offsetTop - pos2;
+   
+       if (newLeft - 216 < viewport.left
+           || newTop < viewport.top
+           || newLeft + rect.width > viewport.right
+           || newTop + rect.height > viewport.bottom
+       ) {
+         // the element will hit the boundary, do nothing...
+       } else {
+         // set the element's new position:
+         elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+         elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+       }
+     }
+   
+     function closeDragElement() {
+       /* stop moving when mouse button is released:*/
+       document.onmouseup = null;
+       document.onmousemove = null;
+     }
+   }
